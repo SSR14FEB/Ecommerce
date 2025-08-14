af79bcb93e7ca248b532d42b3441d684afe06207
SSR14FEB <instaheri6@gmail.com>
2025-08-14 19:15:00 +0530

    test: complete backend setup

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,2 +1,13 @@
-import express from 'express';
+import express from 'express'
 const app = express();
+ app.get('/',(req,res)=>{
+    res.json({
+        "status":"200",
+        "massage":"backend setup is completed"
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("hello backend")
+})
+console.log("testing backend setup");
